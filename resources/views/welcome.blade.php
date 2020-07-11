3293dff691f367f1af0938b972efc71261196905
arihazard-handsome <54012276+arihazard-handsome@users.noreply.github.com>
2020-07-11 11:32:59 +0700

    commit from mac

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel (SAMO PROJECT)
                 </div>
 
                 <div class="links">
